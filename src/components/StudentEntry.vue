8fbf1e17da59d4ad5f89ac61b9043790d9f2d6de
Benjamin Bulis <beni1999@gmx.at>
2021-04-13 15:16:32 +0200

    Working on StudentEntry Component

---
+++ src/components/StudentEntry.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="student">
+        <div class="col s12 m6">
+            <div class="card blue-grey darken-1">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StudentEntry",
+    props: {
+        id: Number,
+        firstname: String,
+        lastname: String,
+        age: Number,
+        phone: String
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
